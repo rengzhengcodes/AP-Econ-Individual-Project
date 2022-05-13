4ffb4da7831674a682a7c8c1d333ec796015872b
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-13 14:19:24 -0400

    imported stuff, maybe

--- measuring_wellbeing.R
+++ measuring_wellbeing.R
@@ -5,3 +5,12 @@ library(readxl)
 library(tidyverse)
 library(reshape2)
 
+# imports the data from Excel file | https://www.core-econ.org/doing-economics/book/text/04-03.html#r-walk-through-41-importing-the-excel-file-xlsx-or-xls-format-into-r
+setwd("./")
+UN = read_excel(# file name
+                "Download-GDPPCgrowth-USD-countries.xlsx",
+                # sheet name
+                "Download-GrowthRateGDPPC-USD",
+                # number of rows to skip
+                skip = 3
+                )
