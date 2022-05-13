baaf5bdd5712395fed221d3fdaf02fea06843cb2
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-13 14:33:34 -0400

    created project

--- measuring_wellbeing.R
+++ measuring_wellbeing.R
@@ -6,11 +6,15 @@ library(tidyverse)
 library(reshape2)
 
 # imports the data from Excel file | https://www.core-econ.org/doing-economics/book/text/04-03.html#r-walk-through-41-importing-the-excel-file-xlsx-or-xls-format-into-r
-setwd("./")
+setwd("C:/Users/theni/OneDrive/Documents/Coding/Repos/AP-Econ-Individual-Project")
+getwd()
 UN = read_excel(# file name
-                "Download-GDPPCgrowth-USD-countries.xlsx",
+                "./Download-GDPPCgrowth-USD-countries.xlsx",
                 # sheet name
                 "Download-GrowthRateGDPPC-USD",
                 # number of rows to skip
-                skip = 3
+                skip = 2
                 )
+
+head(UN)
+
