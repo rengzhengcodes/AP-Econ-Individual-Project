3a8a6f4df3fcc6f36f38e0a4e0b079f55af99aaa
rengzhengcodes <rzhengcontacts@gmail.com>
2022-05-13 14:44:03 -0400

    correct dataset

--- measuring_wellbeing.R
+++ measuring_wellbeing.R
@@ -3,13 +3,12 @@ install.packages(c("readxl", "tidyverse", "reshape2"))
 # activates those libraries
 library(readxl)
 library(tidyverse)
-library(reshape2)
 
 # imports the data from Excel file | https://www.core-econ.org/doing-economics/book/text/04-03.html#r-walk-through-41-importing-the-excel-file-xlsx-or-xls-format-into-r
 setwd("C:/Users/theni/OneDrive/Documents/Coding/Repos/AP-Econ-Individual-Project")
 getwd()
 UN = read_excel(# file name
-                "./Download-GDPPCgrowth-USD-countries.xlsx",
+                "./Download-GDPconstant-USD-countries.xlsx",
                 # sheet name
                 "Download-GrowthRateGDPPC-USD",
                 # number of rows to skip
@@ -18,3 +17,13 @@ UN = read_excel(# file name
 
 head(UN)
 
+# frequency table, also making data long
+library(reshape2)
+wide_UN <- UN
+# keeping all data except for Country ID
+wide_UN = wide_UN[, -1]
+
+# id.vars are the names of the column variables
+long_UN = melt(wide_UN, id.vars = c("Country", "IndicatorName"), value.vars = 4:ncol(UN))
+
+head(long_UN)
